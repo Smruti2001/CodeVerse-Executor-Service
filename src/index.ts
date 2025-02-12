40c812b318299ac271fc3294bdba09eaa1fcda9f
Smruti Mahapatra <getsmruti03@gmail.com>
2025-02-13 02:14:29 +0530

    Feat: Integrated BillBoard UI

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import serverConfig from "./config/serverConfig";
 import apiRouter from "./routes";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import SampleWorker from "./workers/sampleWorker";
+import serverAdapter from "./config/bullBoardConfig";
 
 const app = express();
 
 app.use('/api', apiRouter);
+app.use('/ui', serverAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server started at PORT ${serverConfig.PORT}`);
+    console.log(`Follow the link to access BullBoard dashboard: http://localhost:${serverConfig.PORT}/ui`);
 
     SampleWorker('SampleQueue');
 
