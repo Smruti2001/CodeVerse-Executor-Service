6215390036e38da3543885881c64bb5c972512f8
Smruti Mahapatra <getsmruti03@gmail.com>
2025-03-02 01:09:48 +0530

    Added generic code creator

---
+++ src/utils/codeCreator.ts
@@ -0,0 +1,11 @@
+export default function codeCreator(startingCode: string, midCode: string, endCode: string): string {
+    return `
+    
+    ${startingCode}
+
+    ${midCode}
+
+    ${endCode}
+    
+    `
+}
